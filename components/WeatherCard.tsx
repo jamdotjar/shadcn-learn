b44a585557b1111337e498245efd5fd0c0caf378
jamdotjar <darkestknight3d@outlook.com>
2023-09-16 16:27:32 -0700

    bad cards

--- components/WeatherCard.tsx
+++ components/WeatherCard.tsx
@@ -8,7 +8,7 @@ function WeatherDisplay() {
     useEffect(()=>{
         const getWeather =async () => 
         {
-            const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=49.3164&longitude=-123.0693&hourly=temperature_2m,apparent_temperature,precipitation&current_weather=true&timezone=America%2FLos_Angeles")
+            const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=49.3164&longitude=-123.0693&hourly=precipitation_probability&current_weather=true&timeformat=unixtime&timezone=America%2FLos_Angeles&forecast_days=1")
             const data = await res.json()
             console.log(data)
             setWeatherData(data)
@@ -16,30 +16,52 @@ function WeatherDisplay() {
         getWeather()
 }, [])
     
-    return(
-        <div>
-            {weatherData ? (
-                <div className="text-lg font-semibold">
-                    {weatherData.current_weather}
+        return(
+            <>
+             {weatherData ? (
+                <div className="flex flex-wrap ">
+                    <div className="p-4 md:w-1/3 sm:w-full">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Temperature</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <CardDescription>
+                                    {weatherData.current_weather.temperature}°
+                                </CardDescription>
+                            </CardContent>
+                        </Card>
+                    </div>
+                    <div className="p-4 md:w-1/3 sm:w-full">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Precipitation Probability</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <CardDescription>
+                                    {weatherData.hourly.precipitation_probability[weatherData.hourly.precipitation_probability.length - 1]} per-cent chance of precipitation
+                                </CardDescription>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
+
             ) : (
-                <div>Loading...</div>
+                    <div>Loading...</div>
             )}
-        </div>
-    )
+            </>
+        )
+             
+    
 }
 
 export default function WeatherCard() {
 
     return (
-        <Card className="w-[350px]">
-          <CardHeader>
-            <CardTitle>heres the weather</CardTitle>
-            <CardDescription>Deploy your new project in one-click.</CardDescription>
-          </CardHeader>
-          <CardContent>
+        
             <WeatherDisplay/>
-          </CardContent>
+          
 
-        </Card>
+        
       )
 }
