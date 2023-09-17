aca9ca149dc8047fb1410dc1ce631b4335dd5644
jam.jar <finnhofbauer13@icloud.com>
2023-09-16 17:27:42 -0700

    redid UI added cards

--- app/page.tsx
+++ app/page.tsx
@@ -15,20 +15,21 @@ import {
   NavigationMenuViewport,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
-import WeatherCard from '@/components/WeatherCard'
+import{ WeatherDisplay } from '@/components/WeatherCard'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="z-10  border-radius-50 w-full items-center justify-between font-mono text-sm lg:flex bg-zinc-100 p-4">
-        <h1 className='text-4xl font-bold'>Bad App</h1>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-hello
-        </div>
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <div className="z-10  border-radius-50 w-full items-center justify-between font-mono text-sm lg:flex bg-zinc-100 p-8">
+        <h1 className='text-6xl font-bold'>Bad App</h1>
+        
       </div>
-      <div className="mx-auto w-4/5 text-center flex items-center justify-center h-screen">
-      <WeatherCard/>
+      <div className=' w-3/5 h-screen mx-auto '>
+        North Vancouver
+        <WeatherDisplay/>
       </div>
+      
+
 
 
     </main>
--- components/WeatherCard.tsx
+++ components/WeatherCard.tsx
@@ -3,12 +3,12 @@ import { Button } from "./ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
 import { useEffect, useState } from "react";
 
-function WeatherDisplay() {
+export function WeatherDisplay() {
     const [weatherData, setWeatherData] = useState(null)
     useEffect(()=>{
         const getWeather =async () => 
         {
-            const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=49.3164&longitude=-123.0693&hourly=precipitation_probability&current_weather=true&timeformat=unixtime&timezone=America%2FLos_Angeles&forecast_days=1")
+            const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=49.3164&longitude=-123.0693&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation_probability,precipitation&current_weather=true")
             const data = await res.json()
             console.log(data)
             setWeatherData(data)
@@ -19,31 +19,42 @@ function WeatherDisplay() {
         return(
             <>
              {weatherData ? (
-                <div className="flex flex-wrap ">
-                    <div className="p-4 md:w-1/3 sm:w-full">
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>Temperature</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                <CardDescription>
-                                    {weatherData.current_weather.temperature}°
-                                </CardDescription>
-                            </CardContent>
-                        </Card>
-                    </div>
-                    <div className="p-4 md:w-1/3 sm:w-full">
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>Precipitation Probability</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                <CardDescription>
-                                    {weatherData.hourly.precipitation_probability[weatherData.hourly.precipitation_probability.length - 1]} per-cent chance of precipitation
-                                </CardDescription>
-                            </CardContent>
-                        </Card>
-                    </div>
+                <div className="flex flex-wrap">
+                   <Card className="m-2 text-center">
+                    <CardHeader>
+                        <CardTitle>
+                          Temperature
+                        </CardTitle>
+                        
+                    </CardHeader>
+                        <CardContent>
+                            <div className="text-5xl"> {weatherData?.current_weather?.temperature}°</div>
+                          
+                            <div className="text-2xl text-light text-gray-600">feels like: {weatherData?.current_weather?.temperature}</div>
+                        </CardContent>
+                    </Card>
+                    <Card className="m-2">
+                    <CardHeader>
+                        <CardTitle>
+                          Temperature
+                        </CardTitle>
+                        
+                    </CardHeader>
+                        <CardContent>
+                            enough to MELT your socks offx
+                        </CardContent>
+                    </Card>
+                    <Card className="m-2">
+                    <CardHeader>
+                        <CardTitle>
+                          Temperature
+                        </CardTitle>
+                        
+                    </CardHeader>
+                        <CardContent>
+                            enough to MELT your socks offx
+                        </CardContent>
+                    </Card>
                 </div>
 
             ) : (
@@ -55,13 +66,3 @@ function WeatherDisplay() {
     
 }
 
-export default function WeatherCard() {
-
-    return (
-        
-            <WeatherDisplay/>
-          
-
-        
-      )
-}
