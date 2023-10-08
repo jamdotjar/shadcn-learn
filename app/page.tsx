34d67c84b53e4c3f54aeab2dcfd24aa82f24d84a
jam.jar <finnhofbauer13@icloud.com>
2023-10-07 17:33:01 -0700

    added geolocation

--- app/page.tsx
+++ app/page.tsx
@@ -16,16 +16,16 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 import{ WeatherDisplay } from '@/components/WeatherCard'
+import { DarkModeButton } from 'components/darkModeToggle.tsx';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
-      <div className="z-10  border-radius-50 w-full items-center justify-between font-mono text-sm lg:flex bg-zinc-100 p-8">
+      <div className="z-10  border-radius-50 w-full items-center justify-between font-mono text-sm lg:flex bg-zinc-100 dark:bg-gray-800 p-8">
         <h1 className='text-6xl font-bold'>Bad App</h1>
-        
+        <DarkModeButton />
       </div>
       <div className=' w-3/5 h-screen mx-auto '>
-        North Vancouver
         <WeatherDisplay/>
       </div>
       
@@ -35,3 +35,4 @@ export default function Home() {
     </main>
   )
 }
+
--- components/WeatherCard.tsx
+++ components/WeatherCard.tsx
@@ -3,6 +3,7 @@ import { Button } from "./ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
 import { useEffect, useState } from "react";
 import { LineChart, Line, CartesianGrid,Area, XAxis, YAxis, Tooltip, AreaChart } from 'recharts';
+
 function getHourlyParameter(weatherData, parameter, hour) {
     return weatherData.hourly[parameter][hour];
 }
@@ -21,19 +22,47 @@ function CustomTooltip({ payload, label, active }) {
 
 export function WeatherDisplay() {
     const [weatherData, setWeatherData] = useState(null)
+    const [location, setLocation] = useState({lat: null, lon: null})
 
+    
     useEffect(()=>{
         
         const getWeather =async () => 
         {
-            const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=49.3164&longitude=-123.0693&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation_probability,precipitation&current_weather=true&timezone=America%2FLos_Angeles&forecast_days=3")
-            const data = await res.json()
-            console.log(data)
-            setWeatherData(data)
+
+            if ("geolocation" in navigator){
+                navigator.geolocation.getCurrentPosition(async position => {
+                    const lat = position.coords.latitude;
+                    const lon = position.coords.longitude;
+                    setLocation({lat, lon});
+                    const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation_probability,precipitation&current_weather=true&timezone=America%2FLos_Angeles&forecast_days=3`)
+                    const data = await res.json()
+                    console.log(data)
+                    setWeatherData(data)
+                });
+            }
+            else{
+                const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=49.3164&longitude=-123.0693&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation_probability,precipitation&current_weather=true&timezone=America%2FLos_Angeles&forecast_days=3")
+                const data = await res.json()
+                console.log(data)
+                setWeatherData(data)
+            }
+            
             
         }
         getWeather()
 }, [])
+useEffect(() => {
+    const getCityName = async (lat, lon) => {
+        const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`);
+        const data = await response.json();
+        return data.address.city;
+    }
+
+    if (location.lat && location.lon) {
+        getCityName(location.lat, location.lon).then(city => setLocation({ ...location, city }));
+    }
+}, [location.lat, location.lon]);
 const currentHour = new Date().getHours();
 
 // Prepare the data for the chart
@@ -51,8 +80,10 @@ for (let i = 0; i < 12; i++) {
         return(
             
             <>
+
              {weatherData ? (
-                
+                <>
+               {location.city}
                 <div  className="flex flex-wrap">
                    <Card className="m-2 text-center">
                     <CardHeader>
@@ -62,9 +93,9 @@ for (let i = 0; i < 12; i++) {
                         
                     </CardHeader>
                         <CardContent>
-                            <div className="text-5xl"> {weatherData?.current_weather?.temperature}°</div>
+                            <div className="text-5xl dark:text-accent"> {weatherData?.current_weather?.temperature}°</div>
                                 {weatherData?.current_weather?.temperature !== getHourlyParameter(weatherData, 'apparent_temperature', currentHour) && 
-                                <div className="text-l font-light text-gray-600">Feels like: {getHourlyParameter(weatherData, 'apparent_temperature', currentHour)}°</div>
+                                <div className="text-l font-light text-gray-600 dark:text-accent">Feels like: {getHourlyParameter(weatherData, 'apparent_temperature', currentHour)}°</div>
                             }
                             
                         </CardContent>
@@ -78,10 +109,11 @@ for (let i = 0; i < 12; i++) {
                         
                     </CardHeader>
                         <CardContent>
-                        <div className="text-5xl">
+                        <div className="text-5xl dark:text-accent">
+
                             {getHourlyParameter(weatherData, 'precipitation_probability', currentHour)}%
                             </div>
-                            <div className="text-l font-light text-gray-600">Chance of precipitation</div>
+                            <div className="text-l font-light text-gray-600 dark:text-accent">Chance of precipitation</div>
                         </CardContent>
                     </Card>
                     <Card className="m-2">
@@ -93,15 +125,18 @@ for (let i = 0; i < 12; i++) {
                     </CardHeader>
                         <CardContent>
                         <AreaChart width={500} height={300} data={forecastData}>
-                        <Area type="monotone" dataKey={(data) => Number(data.temperature.slice(0, -1))} stroke="#0000FF" fill="#ADD8E6" />
-    <CartesianGrid stroke="#ccc" />
-    <XAxis dataKey="time" stroke="#0000FF" />
-    <YAxis />
-    <Tooltip />
-</AreaChart>
+                        <Area type="monotone" dataKey={(data) => Number(data.temperature.slice(0, -1))} stroke="#808080" fill="#C0C0C0" />
+                        <CartesianGrid stroke="#ccc" />
+                        <XAxis dataKey="time" stroke="#808080" />
+                        <YAxis/>
+                       <Tooltip/>
+                       </AreaChart>
                         </CardContent>
+
                     </Card>
+                    
                 </div>
+                </>
 
             ) : (
                     <div>Loading...</div>
@@ -111,4 +146,5 @@ for (let i = 0; i < 12; i++) {
              
     
 }
+
 
